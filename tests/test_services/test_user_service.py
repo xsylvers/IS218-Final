3b2560603ac10af8b01acfc7d26586deef069233
xsylvers <157817423+xsylvers@users.noreply.github.com>
2024-12-18 15:42:46 -0500

    Added comprehensive tests for user service functionality with new list non professionals via api and also a professional status update via email notification

--- tests/test_services/test_user_service.py
+++ tests/test_services/test_user_service.py
@@ -8,7 +8,7 @@ from app.utils.nickname_gen import generate_nickname
 
 pytestmark = pytest.mark.asyncio
 
-# Test creating a user with valid data
+# This test is creating a user with valid data
 async def test_create_user_with_valid_data(db_session, email_service):
     user_data = {
         "nickname": generate_nickname(),
@@ -20,7 +20,7 @@ async def test_create_user_with_valid_data(db_session, email_service):
     assert user is not None
     assert user.email == user_data["email"]
 
-# Test creating a user with invalid data
+# This test is creating a user with invalid data
 async def test_create_user_with_invalid_data(db_session, email_service):
     user_data = {
         "nickname": "",  # Invalid nickname
@@ -30,7 +30,7 @@ async def test_create_user_with_invalid_data(db_session, email_service):
     user = await UserService.create(db_session, user_data, email_service)
     assert user is None
 
-# Test fetching a user by ID when the user exists
+# This test is fetching data for a user by ID when the user exists
 async def test_get_by_id_user_exists(db_session, user):
     retrieved_user = await UserService.get_by_id(db_session, user.id)
     assert retrieved_user.id == user.id
@@ -41,50 +41,50 @@ async def test_get_by_id_user_does_not_exist(db_session):
     retrieved_user = await UserService.get_by_id(db_session, non_existent_user_id)
     assert retrieved_user is None
 
-# Test fetching a user by nickname when the user exists
+# This test is fetching for a user by nickname when the user exists
 async def test_get_by_nickname_user_exists(db_session, user):
     retrieved_user = await UserService.get_by_nickname(db_session, user.nickname)
     assert retrieved_user.nickname == user.nickname
 
-# Test fetching a user by nickname when the user does not exist
+# This test is fetching data for a user by nickname when the user does not exist
 async def test_get_by_nickname_user_does_not_exist(db_session):
     retrieved_user = await UserService.get_by_nickname(db_session, "non_existent_nickname")
     assert retrieved_user is None
 
-# Test fetching a user by email when the user exists
+# This test is fetching data for a user by email when the user exists
 async def test_get_by_email_user_exists(db_session, user):
     retrieved_user = await UserService.get_by_email(db_session, user.email)
     assert retrieved_user.email == user.email
 
-# Test fetching a user by email when the user does not exist
+# This test is fetching data for a user by email when the user does not exist
 async def test_get_by_email_user_does_not_exist(db_session):
     retrieved_user = await UserService.get_by_email(db_session, "non_existent_email@example.com")
     assert retrieved_user is None
 
-# Test updating a user with valid data
+# This test is updating a user with valid data
 async def test_update_user_valid_data(db_session, user):
     new_email = "updated_email@example.com"
     updated_user = await UserService.update(db_session, user.id, {"email": new_email})
     assert updated_user is not None
     assert updated_user.email == new_email
 
-# Test updating a user with invalid data
+# This test is updating a user with invalid data
 async def test_update_user_invalid_data(db_session, user):
     updated_user = await UserService.update(db_session, user.id, {"email": "invalidemail"})
     assert updated_user is None
 
-# Test deleting a user who exists
+# This test is deleting a user who exists
 async def test_delete_user_exists(db_session, user):
     deletion_success = await UserService.delete(db_session, user.id)
     assert deletion_success is True
 
-# Test attempting to delete a user who does not exist
+# This test is attempting to delete a user who does not exist
 async def test_delete_user_does_not_exist(db_session):
     non_existent_user_id = "non-existent-id"
     deletion_success = await UserService.delete(db_session, non_existent_user_id)
     assert deletion_success is False
 
-# Test listing users with pagination
+# This test is listing users with pagination
 async def test_list_users_with_pagination(db_session, users_with_same_role_50_users):
     users_page_1 = await UserService.list_users(db_session, skip=0, limit=10)
     users_page_2 = await UserService.list_users(db_session, skip=10, limit=10)
@@ -92,7 +92,15 @@ async def test_list_users_with_pagination(db_session, users_with_same_role_50_us
     assert len(users_page_2) == 10
     assert users_page_1[0].id != users_page_2[0].id
 
-# Test registering a user with valid data
+# This test is listing non professional users
+async def test_list_users_non_professional(db_session, users_with_same_role_50_users):
+    users_page_1 = await UserService.list_users(db_session, skip=0, limit=10)
+    users_page_2 = await UserService.list_users(db_session, skip=10, limit=10)
+    assert len(users_page_1) == 10
+    assert len(users_page_2) == 10
+    assert users_page_1[0].id != users_page_2[0].id
+
+# This test is to attempt a registeration for a user with valid data
 async def test_register_user_with_valid_data(db_session, email_service):
     user_data = {
         "nickname": generate_nickname(),
@@ -104,7 +112,7 @@ async def test_register_user_with_valid_data(db_session, email_service):
     assert user is not None
     assert user.email == user_data["email"]
 
-# Test attempting to register a user with invalid data
+# This test is to attempt a registertation for a user with invalid data
 async def test_register_user_with_invalid_data(db_session, email_service):
     user_data = {
         "email": "registerinvalidemail",  # Invalid email
@@ -113,7 +121,7 @@ async def test_register_user_with_invalid_data(db_session, email_service):
     user = await UserService.register_user(db_session, user_data, email_service)
     assert user is None
 
-# Test successful user login
+# This is testing a successful user login
 async def test_login_user_successful(db_session, verified_user):
     user_data = {
         "email": verified_user.email,
@@ -122,17 +130,17 @@ async def test_login_user_successful(db_session, verified_user):
     logged_in_user = await UserService.login_user(db_session, user_data["email"], user_data["password"])
     assert logged_in_user is not None
 
-# Test user login with incorrect email
+# This is test the user loggin in with an incorrect email
 async def test_login_user_incorrect_email(db_session):
     user = await UserService.login_user(db_session, "nonexistentuser@noway.com", "Password123!")
     assert user is None
 
-# Test user login with incorrect password
+# This is testing the user loggin in with an incorrect password
 async def test_login_user_incorrect_password(db_session, user):
     user = await UserService.login_user(db_session, user.email, "IncorrectPassword!")
     assert user is None
 
-# Test account lock after maximum failed login attempts
+# This is testing the account to lock after maximum failed login attempts
 async def test_account_lock_after_failed_logins(db_session, verified_user):
     max_login_attempts = get_settings().max_login_attempts
     for _ in range(max_login_attempts):
@@ -141,13 +149,13 @@ async def test_account_lock_after_failed_logins(db_session, verified_user):
     is_locked = await UserService.is_account_locked(db_session, verified_user.email)
     assert is_locked, "The account should be locked after the maximum number of failed login attempts."
 
-# Test resetting a user's password
+# This is testing on resetting a user's password
 async def test_reset_password(db_session, user):
     new_password = "NewPassword123!"
     reset_success = await UserService.reset_password(db_session, user.id, new_password)
     assert reset_success is True
 
-# Test verifying a user's email
+# This is testing a verificaiton to the user's email
 async def test_verify_email_with_token(db_session, user):
     token = "valid_token_example"  # This should be set in your user setup if it depends on a real token
     user.verification_token = token  # Simulating setting the token in the database
@@ -155,9 +163,23 @@ async def test_verify_email_with_token(db_session, user):
     result = await UserService.verify_email_with_token(db_session, user.id, token)
     assert result is True
 
-# Test unlocking a user's account
+# This is testing to unlock a user's account
 async def test_unlock_user_account(db_session, locked_user):
     unlocked = await UserService.unlock_user_account(db_session, locked_user.id)
     assert unlocked, "The account should be unlocked"
     refreshed_user = await UserService.get_by_id(db_session, locked_user.id)
     assert not refreshed_user.is_locked, "The user should no longer be locked"
+
+#This is testing the user professional status update
+async def test_update_user_professional_status_valid(db_session, user, email_service):
+    new_is_professional = True
+    updated_user = await UserService.update_is_now_a_professional(db_session, user.id, new_is_professional, email_service)
+    assert updated_user is not None
+    assert updated_user.is_now_a_professional == new_is_professional
+    assert updated_user.professional_status_updated_at is not None
+
+async def test_update_user_professional_status_invalid(db_session, user, email_service):
+    new_is_professional = True
+    updated_user = await UserService.update_is_now_a_professional(db_session, 1234, new_is_now_a_professional, email_service)
+    assert updated_user is None
+
